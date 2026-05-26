42fcd9cd610565653f94cd2e36d014893e04cb4a
SimpleSimpleSimples <136209651712@163.com>
2026-05-26 21:22:13 +0800

    update message struct

---
+++ byclaw-be/src/main/java/com/iwhalecloud/byai/state/common/dto/MessageStructDto.java
@@ -0,0 +1,20 @@
+package com.iwhalecloud.byai.state.common.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author he.duming
+ * @date 2026-05-26 20:31:09
+ * @description TODO
+ */
+@Getter
+@Setter
+public class MessageStructDto {
+
+    private Long messageId;
+
+    private String id;
+
+    private String content;
+}
