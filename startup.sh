251f34a0fac15b3f47400977d043428dc67145f6
ruru5697 <429611948@qq.com>
2026-06-08 17:31:29 +0800

    chore: soft-delete test fixtures, docs/superpowers, and misc files from repo

--- startup.sh
+++
@@ -1,4 +0,0 @@
-docker build --build-arg HTTP_PROXY=http://host.docker.internal:7890 --build-arg HTTPS_PROXY=http://host.docker.internal:7890 -t byclaw-qa:test .
-
-docker run -it --rm --name byclaw-qa-worker --env-file=/Users/jialangli/code/workspace/byclaw-all/byclaw-qa/.env.docker byclaw-qa:test worker
-docker run -it --rm --name byclaw-qa-api -p 8000:8000 --env-file=/Users/jialangli/code/workspace/byclaw-all/byclaw-qa/.env.docker byclaw-qa:test api
