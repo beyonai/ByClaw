75bf4381a5dae37eca6c0254305277408846a9d2
liu.yafei <liu.yafei@foxmail.com>
2026-06-25 11:30:42 +0800

    docs(locales): update chinese locale wording for troubleshooting
    modify the troubleshoot related translation texts to more natural chinese expressions, adjust the prompt wording to be clearer

--- byclaw-fe/src/locales/zh-CN.ts
+++ byclaw-fe/src/locales/zh-CN.ts
@@ -553,9 +553,9 @@ export default {
   'messageForm.pleaseUpload': '请上传{files}',
 
   'messageList.deleteMessageConfirm': '确定删除该消息吗？',
-  'messageList.troubleshoot': '排障',
+  'messageList.troubleshoot': '运维',
   'messageList.troubleshootTooltip': '利用百应运维数字员工，帮您排除故障',
-  'messageList.troubleshootPrompt': '当前消息的traceId为{traceId}，请帮我排除故障',
+  'messageList.troubleshootPrompt': '需要排查的trace_id={traceId}，\n',
 
   'citeRender.title': '引用消息',
   'citeRender.deleteConfirm': '确定删除该引用吗？',
