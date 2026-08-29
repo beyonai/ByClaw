69dc8abfcedaeca9e452f05b977f4ad05abcb3de
korvo <cxf837385194@gmail.com>
2026-08-29 11:07:37 +0800

    feat(chat): surface latest task plans

--- byclaw-fe/src/components/MessageList/useRender.tsx
+++ byclaw-fe/src/components/MessageList/useRender.tsx
@@ -13,7 +13,6 @@ import DualBallLoading from '@/components/Loading/DualBallLoading';
 import WaveBallLoading from '@/components/Loading/WaveBallLoading';
 import CiteRender from '@/components/MessageList/components/CiteRender';
 import FileRender from '@/components/MessageList/components/FileRender';
-import TaskExecutionPlan from '@/components/MessageList/components/TaskExecutionPlan';
 import NotSupport from '@/components/NotSupport';
 
 import ThumbUpContent from './components/AnswerActions/ThumbUp/content';
@@ -417,7 +416,6 @@ export default function useRender({
                 [styles.pureText]: fromOtherUser && usage !== '4',
               })}
             >
-              {fromBeyond && msg.taskPlan && <TaskExecutionPlan taskPlan={msg.taskPlan} />}
               {isV2Message(msg) ? (
                 <MsgRendererV2 msg={msg} updateMessage={updateMessage} hideThinking={param?.hideThinking} />
               ) : (
