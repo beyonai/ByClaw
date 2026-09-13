9c1c949e28e163490a1f086871553990454dbfec
Pliosauroideas <Pliosauroide@protonmail.com>
2026-09-13 11:51:27 +0800

    feat(fe): remove view/object center routes and pages
    Delete the /viewCenter and /objectCenter routes and their page components. Part of retiring the VIEW/OBJECT resource capability (devflow 0012); 0013/0014/0018 ship as one batch. (0013)

--- byclaw-fe/src/pages/viewCenter/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Resources from '@/components/Resources';
-
-const ViewCenter: React.FC = () => {
-  return <Resources resourceType="VIEW" />;
-};
-
-export default ViewCenter;
